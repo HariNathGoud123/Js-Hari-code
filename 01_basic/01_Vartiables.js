5e4264bf99ea66d9aa1b8a63b98c66369c1f1899
HariNathGoud123 <146013645+HariNathGoud123@users.noreply.github.com>
2023-09-27 10:22:58 +0000

    variables

---
+++ 01_basic/01_Vartiables.js
@@ -0,0 +1,5 @@
+const account_Id=123843;
+let email="hari@123";
+var city="sirvel";
+//console.log(account_Id);
+console.table([account_Id,email,city]);
---
+++ practice/variable.js
@@ -0,0 +1,7 @@
+console.log("hello");
+const account_Id=123843;
+let email="hari@123";
+var city="sirvel";
+//console.log(account_Id);
+console.table([account_Id,email,city]);
+console.log(typeof undefined)
